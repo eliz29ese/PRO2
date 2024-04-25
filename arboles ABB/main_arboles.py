0845d8229a88218cad14a0bf40f7249125d45dd8
eliz29ese <158051658+eliz29ese@users.noreply.github.com>
2024-04-25 11:35:47 +0200

    Update main_arboles.py

--- arboles ABB/main_arboles.py
+++ arboles ABB/main_arboles.py
@@ -5,99 +5,278 @@ Santrich Escalona, Elizabet
 Rodríguez Polín, Isabel
 (isabel.rodriguezp@udc.es)
 """
+from avl_tree import AVL
 
-class Course: 
-    def __init__(self, name: str, duration: float , students: int, level: str, language: str, price: float):
+class Course:
+    def __init__(self, name: str, duration: float, students: int, level: str, language: str, price: float):
         """
         Creates a Course with the given attributes.
 
         Parameters
         ----------
         name : str
-            DESCRIPTION.
+            The name of the course.
         duration : float
-            DESCRIPTION.
+            The duration of the course (hours).
         students : int
-            DESCRIPTION.
+            The number of students enrolled in the course.
         level : str
-            DESCRIPTION.
+            The level of the course.
         language : str
-            DESCRIPTION.
+            The language of the course.
         price : float
-            DESCRIPTION.
+            The price of the course.
 
         Returns
         -------
-        None.
-
+        None
         """
-        self._name = name 
-        self._duration = duration 
+        self._name = name
+        self._duration = duration
         self._students = students
-        self._level = level 
+        self._level = level
         self._language = language
-        self._price = price 
+        self._price = price
 
     @property
     def name(self):
         """
-        Gets the name of the course. 
-        
+        Gets the name of the course.
+
         Returns
         -------
         str
-            The name of the course 
+            The name of the course
         """
-        return self._name 
- 
+        return self._name
+
     @name.setter
     def name(self, value: str):
         """
         Set the name of the course.
- 
+
         Parameters
         ----------
-        value : str 
+        value : str
             The new name of the course.
- 
+
         Raises
         ------
         ValueError
             If the provided value is not a non-empty string.
         """
-        if isinstance(value, str) and len(value) != 0 :
+        if isinstance(value, str) and len(value) != 0:
             self._name = value
         else:
-            raise ValueError("The name of the course must be a non empty string")
+            raise ValueError("The name of the course must be a non-empty string")
 
     @property
     def duration(self):
         """
-        Gets the duration of the course. 
-        
+        Gets the duration of the course.
+
         Returns
         -------
         float
-            The duration of the course (hours). 
+            The duration of the course (hours).
         """
-        return self._duration  
- 
+        return self._duration
+
     @duration.setter
     def duration(self, value: float):
         """
         Set the duration of the course.
- 
+
         Parameters
         ----------
-        value : float 
-            The new hours of the course duration 
- 
+        value : float
+            The new hours of the course duration
+
         Raises
         ------
         ValueError
-            If the provided value is not a float grater than 0
+            If the provided value is not a float greater than 0
         """
-        if isinstance(value, str) and len(value) != 0 :
+        if isinstance(value, float) and value > 0:
             self._duration = value
         else:
-            raise ValueError("The name of the course must be a non empty string")
+            raise ValueError("The hours of duration must be a float greater than 0")
+
+    @property
+    def students(self):
+        """
+        Gets the number of students enrolled in the course.
+
+        Returns
+        -------
+        int
+            The number of students enrolled in the course
+        """
+        return self._students
+
+    @students.setter
+    def students(self, value: int):
+        """
+        Set the number of students enrolled in the course.
+
+        Parameters
+        ----------
+        value : int
+            The new number of students enrolled in the course.
+
+        Raises
+        ------
+        ValueError
+            If the provided value is not a non-negative integer.
+        """
+        if isinstance(value, int) and value >= 0:
+            self._students = value
+        else:
+            raise ValueError("Number of students must be a non-negative integer")
+
+    @property
+    def level(self):
+        """
+        Gets the level of the course.
+
+        Returns
+        -------
+        str
+            The level of the course
+        """
+        return self._level
+
+    @level.setter
+    def level(self, value: str):
+        """
+        Set the level of the course.
+
+        Parameters
+        ----------
+        value : str
+            The new level of the course.
+
+        Raises
+        ------
+        ValueError
+            If the provided value is not a non-empty string.
+        """
+        if isinstance(value, str) and len(value) != 0:
+            self._level = value
+        else:
+            raise ValueError("Level must be a non-empty string")
+
+    @property
+    def language(self):
+        """
+        Gets the language of the course.
+
+        Returns
+        -------
+        str
+            The language of the course
+        """
+        return self._language
+
+    @language.setter
+    def language(self, value: str):
+        """
+        Set the language of the course.
+
+        Parameters
+        ----------
+        value : str
+            The new language of the course.
+
+        Raises
+        ------
+        ValueError
+            If the provided value is not a non-empty string.
+        """
+        if isinstance(value, str) and len(value) != 0:
+            self._language = value
+        else:
+            raise ValueError("Language must be a non-empty string")
+
+    @property
+    def price(self):
+        """
+        Gets the price of the course.
+
+        Returns
+        -------
+        float
+            The price of the course
+        """
+        return self._price
+
+    @price.setter
+    def price(self, value: float):
+        """
+        Set the price of the course.
+
+        Parameters
+        ----------
+        value : float
+            The new price of the course.
+
+        Raises
+        ------
+        ValueError
+            If the provided value is not a non-negative float.
+        """
+        if isinstance(value, float) and value >= 0:
+            self._price = value
+        else:
+            raise ValueError("Price must be a non-negative float")
+
+    def __str__(self):
+        """
+        Returns a string representation of the Course.
+
+        Returns
+        -------
+        str
+            A string representing the Course object.
+        """
+        return f"Name: {self.name}, Duration: {self.duration} hours, Students: {self.students}, Level: {self.level}, Language: {self.language}, Price: {self.price}€"
+
+def create_course(cursos_text:str, academy:str )-> None:
+    tree_A = AVL()
+    tree_B = AVL()
+    
+    cursos = cursos_text.split("\n")
+    for line in cursos:
+        if not line.startswith('#'):
+            curso_info = line.split(',')
+            name, duration, students, level, language, price= curso_info 
+            curso = Course(name, duration, students, level, language, price)
+            if academy == 'A':
+                tree_A[curso.name, curso.level, curso.language] = curso
+            else: 
+                tree_B[curso.name, curso.level, curso.language] = curso
+    
+cursos_A = input('Ingrese el nombre del archivo donde se encuentran los cursos de la academia A:')  
+cursos_B = input('Ingrese el nombre del archivo donde se encuentran los cursos de la academia B:')     
+with open(cursos_A, encoding="ISO-8859-1") as f:
+     # With strip(), we ensure that there are no additional spaces, tabs, or newline characters present in the file.
+    cursos_text = f.read().strip()
+    data_film_list = create_course(cursos_text, 'A') 
+with open(cursos_B, encoding="ISO-8859-1") as f:
+     # With strip(), we ensure that there are no additional spaces, tabs, or newline characters present in the file.
+    cursos_text = f.read().strip()
+    data_film_list = create_course(cursos_text, 'B')    
+     
+arbol_1 = AVL()
+arbol_2 = AVL()
+curso_2 = Course('Rust',60,10,'A','Cas',15.00)
+curso_1 = Course('C',120,30,'A','Cas',8.00)
+arbol_1[curso_2.name, curso_2._level, curso_2._language] = curso_2
+arbol_1[curso_1.name, curso_1._level, curso_1._language] = curso_1
+clave_valor = arbol_1.find_max()
+print("La clave menor del árbol es "+ str(clave_valor[0]) + " con valor " + str(clave_valor[1]))
+clave_valor = arbol_1.find_min()
+print("La clave menor del árbol es "+ str(clave_valor[0]) + " con valor " + str(clave_valor[1]))
+del arbol_1[curso_2.name, curso_2._level, curso_2._language]
+clave_valor = arbol_1.find_max()
+print("La clave menor del árbol es "+ str(clave_valor[0]) + " con valor " + str(clave_valor[1]))
